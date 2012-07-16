3c7642efeba58f7e6ae047e87cfa8a9b92815b65
Bucataru Bogdan <bogbuk@gmail.com>
2012-07-16 14:30:19 +0300

    ch config

--- PhpProject1/config.php
+++ PhpProject1/config.php
@@ -2,6 +2,7 @@
 vvserfgegege
 $consumer_key='571781114295.apps.googleusercontent.com';
 $consumer_secret='YDHKBqtqTsfJ3vFJSS8chbMp';
+eferfrfrfrrfrfrf
 $callback='http://www.momuku.com/gauth.php';
 $emails_count='500';
 ?>
